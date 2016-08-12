9789a23890a2c0685dba9300266cdf62eeb2bcff
Josh Nichols <technicalpickles@github.com>
2016-08-12 18:17:58 -0400

    Add setup.py, remove main thing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sleepyq',
+      version='0.1',
+      description='SleepIQ API for Python',
+      url='http://github.com/technicalpickles/sleepyq',
+      author='Josh Nichols',
+      author_email='josh@technicalpickles.com',
+      license='MIT',
+      packages=['sleepyq'],
+      zip_safe=False)
