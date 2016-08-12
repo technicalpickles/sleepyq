13f48af37e3d76cd46969d40b3f26f1f61ee249a
Josh Nichols <technicalpickles@github.com>
2016-08-12 17:18:56 -0400

    start using sessions. add beds and family status

--- sleepyq/__init__.py
+++ sleepyq/__init__.py
@@ -1,27 +1,41 @@
 import requests
 import os
+from IPython import embed
 
 class Sleepyq:
     def __init__(self, login, password):
         self._login = login
         self._password = password
+        self._session = requests.Session()
 
     def login(self):
-        data = {'login': self._login, 'password': self._password}
-        r = requests.put('https://api.sleepiq.sleepnumber.com/rest/login', json=data)
+        if '_k' in self._session.params:
+            del self._session.params['_k']
 
-        self.key = r.json()['key']
-        self.cookies = r.cookies
+        data = {'login': self._login, 'password': self._password}
+        r = self._session.put('https://api.sleepiq.sleepnumber.com/rest/login', json=data)
+
+        self._session.params['_k'] = r.json()['key']
 
     def sleepers(self):
-        url = 'https://api.sleepiq.sleepnumber.com/rest/sleeper?_k=' + self.key
-        r = requests.get(url, cookies=self.cookies)
+        url = 'https://api.sleepiq.sleepnumber.com/rest/sleeper'
+        r = self._session.get(url)
+        return r.json()
+
+    def beds(self):
+        url = 'https://api.sleepiq.sleepnumber.com/rest/bed'
+        r = self._session.get(url)
+        return r.json()
+
+    def bed_family_status(self):
+        url = 'https://api.sleepiq.sleepnumber.com/rest/bed/familyStatus'
+        r = self._session.get(url)
+        #embed()
         return r.json()
 
 if __name__ == "__main__":
     from pprint import pprint
     import dotenv
-    from IPython import embed
 
     dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
     dotenv.load_dotenv(dotenv_path)
@@ -29,5 +43,5 @@ if __name__ == "__main__":
     client = Sleepyq(os.environ['SLEEPIQ_LOGIN'], os.environ['SLEEPIQ_PASSWORD'])
 
     client.login()
-    sleepers = client.sleepers()
-    pprint(sleepers)
+    beds = client.beds()
+    pprint(beds)
