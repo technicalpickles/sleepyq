2d3e87e016ef7d620b043a3afaa3c6ee6929fc17
Josh Nichols <technicalpickles@github.com>
2016-08-14 10:59:48 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='sleepyq',
-      version='0.3',
+      version='0.4',
       description='SleepIQ API for Python',
       long_description=readme(),
       url='http://github.com/technicalpickles/sleepyq',
